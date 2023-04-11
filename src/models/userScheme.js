008db68c31eb92b028e0aaf2bf19506ee39d2d12
YuvalOfek1 <yuvalofek97@gmail.com>
2023-04-11 16:03:13 +0300

    userSchema

---
+++ src/models/userScheme.js
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+
+const userScheme = mongoose.Schema({
+    id: mongoose.Schema.Types.ObjectId,
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        // validate: {
+        //     validator: function(v) {
+        //       // Regex to validate email format
+        //       return /\S+@\S+\.\S+/.test(v);
+        //     },
+        //     message: props => `${props.value} is not a valid email address!`
+        // }
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profilePicture: {
+        type: String,
+    },
+    phoneNumber: {
+        type: String,
+        trim: true
+    },
+    address: {
+        type: {
+            street: {
+            type: String,
+            required: true,
+            trim: true
+            },
+            city: {
+            type: String,
+            required: true,
+            trim: true
+            },
+            zip: {
+            type: String,
+            required: true,
+            trim: true
+            }
+        }
+    },
+    role: {
+        type: String,
+        enum: ['customer', 'restaurantAdmin'],
+        default: 'customer'
+    },
+    orders: {
+        type: [{ref:'order'}],
+    },
+    numOfOrders: {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+    creditCardNumber: {
+        type: String
+    }
+
+
+});
+
+module.exports = mongoose.model('user', userScheme);
