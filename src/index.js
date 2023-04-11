4fc360b96c856d38606210a4980b3f054d169793
Idan Asayag <idanasayag0@gmail.com>
2023-04-11 16:31:23 +0300

    add get route

--- src/index.js
+++ src/index.js
@@ -9,9 +9,8 @@ const app = express();
 app.use(express.json())
 const PORT = 8080;
 
-
-app.get("/" , (req,res)=>{
-    res.send("Express test");
+app.get("/" ,(req,res)=>{
+    res.send("Hello World");
 });
 
 
