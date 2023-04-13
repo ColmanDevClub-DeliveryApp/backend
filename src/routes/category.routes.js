59fa225bbb457a814e35c114cea16bfb2a152449
Idan Asayag <idanasayag0@gmail.com>
2023-04-13 14:42:42 +0300

    fix routes names

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 /**
  * return list of the shops in the category.
  */
-router.get('/:id', (req, res)=>{
-    const {id} = req.params;
-    res.send(`You requested category ${id}`);
+router.get('/:category', (req, res)=>{
+    const {category} = req.params;
+    res.send(`You requested category ${category}`);
 });
 
 export default router;
--- src/routes/restaurant.routes.js
+++ src/routes/restaurant.routes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 /**
  * return the specified restaurant.
  */
-router.get('/:id', (req, res)=>{
-    const {id} = req.params;
-    res.send(`You requested restaurant ${id}`);
+router.get('/:restaurantName', (req, res)=>{
+    const {restaurantName} = req.params;
+    res.send(`You requested restaurant ${restaurantName}`);
 });
 
 export default router;
