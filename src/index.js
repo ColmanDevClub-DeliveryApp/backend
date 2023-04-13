8c18dff80f4178a0cd1f3ad290c9bef47e93d01f
Idan Asayag <idanasayag0@gmail.com>
2023-04-13 21:38:16 +0300

    add notFound, HomePage routes

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ import dotenv from 'dotenv';
 import dishesRoutes from './routes/dishes.routes.js';
 import restaurantRoutes from './routes/restaurant.routes.js';
 import categoryRoutes from './routes/category.routes.js';
+import notFoundRoutes from './routes/notFound.routes.js';
+import homeRoutes from './routes/homePage.routes.js';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 const PORT = 8080;
 
-app.use('/dishes', dishesRoutes)
-app.use('/restaurants', restaurantRoutes)
-app.use('/category', categoryRoutes)
+app.use('/', homeRoutes);
+app.use('/dishes', dishesRoutes);
+app.use('/restaurants', restaurantRoutes);
+app.use('/category', categoryRoutes);
+app.use('*', notFoundRoutes);
 
 
 
