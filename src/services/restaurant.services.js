0d3e509f932cfd5f4a9bb2e8b188f8b034e59f53
Tal Mekler <tal.mekler11@gmail.com>
2023-04-13 23:06:26 +0300

    add rest service

--- src/services/restaurant.services.js
+++ src/services/restaurant.services.js
@@ -2,7 +2,7 @@
 import { Restaurant } from "../models/restScheme"
 
 const getById = (id)=> {
-    return Restaurant.findById(id);
+    return Restaurant.findOne(id);
 }
 const getAll = ()=> {
     return Restaurant.find();
