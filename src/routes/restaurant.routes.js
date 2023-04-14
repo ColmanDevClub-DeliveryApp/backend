992f73c4df799b5716b579bc15faf17fe638cee9
Idan Asayag <idanasayag0@gmail.com>
2023-04-14 10:29:01 +0300

    add try-catch, restaurant routes

--- src/routes/restaurant.routes.js
+++ src/routes/restaurant.routes.js
@@ -1,29 +1,50 @@
 import express from "express";
 import RestaurantApi from "../services/restaurant.services.js";
+import notFoundRoutes from "../routes/notFound.routes.js";
 const router = express.Router();
 
 /**
  * return the specified restaurant.
  */
-
-// todo: add try and catch
 router.get("/", async (req, res) => {
   const { id, name } = req.query;
-  console.log(id);
-  console.log(name);
   if (id) {
-    return res.send(
-      `You requested restaurant by ID: ${await RestaurantApi.getRestaurantById(id)}`
-    );
+    try{
+      const userID = await RestaurantApi.getRestaurantById(id);
+      if(userID){
+        return res.send(
+          `You requested restaurant by ID: ${userID}`
+        );
+      }
+    }catch(e){
+      // here we need to send a 404 error
+      return res.send(
+        `You requested restaurant by ID, but it doesn't exist: ${id}`
+      );
+    }
   }
+
   if (name) {
-    return res.send(
-      `You requested restaurant by name: ${await RestaurantApi.getRestaurantByName(name)}`
-    );
+    try{
+      const restaurantName = await RestaurantApi.getRestaurantByName(name);
+      if(restaurantName){
+        return res.send(
+          `You requested restaurant by name: ${await RestaurantApi.getRestaurantByName(name)}`
+        );
+      }
+    }catch(e){
+        // here we need to send a 404 error
+      return res.send(
+        `You requested restaurant by name, but it doesn't exist: ${name}`
+      );
+    }
   }
+
   res.send(
     `You requested restaurant ${await RestaurantApi.getAllRestaurant()}`
   );
 });
+
+router.get("*", notFoundRoutes);
 
 export default router;
