e45c1332aacea34da3ecd25d182192836c925e86
Idan Asayag <idanasayag0@gmail.com>
2023-04-16 13:34:19 +0300

    add signup route(post)

--- src/routes/homePage.routes.js
+++ src/routes/homePage.routes.js
@@ -1,9 +1,29 @@
 import express from "express"
 
+// import { User } from "../models/userScheme.js"
 const router = express.Router();
 
 router.get('/', (req, res)=>{
     res.send('Home Page');
 });
 
+ 
+/* don't remove this comment until we fix the problem with the ref in our Schema
+router.post('/signup', async (req, res)=>{
+    const {name, email, password, phone} = req.body;
+    const user = new User({
+        name,
+        email,
+        password,
+        phone
+    })
+
+    try {
+        const newUser = await user.save();
+        res.send(newUser);
+    } catch (error) {
+        console.log(error);
+    }
+});
+*/
 export default router;
