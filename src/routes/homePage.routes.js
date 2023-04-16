6e5d14e216a3f5dc2b28e50cfbe703814400eebf
Idan Asayag <idanasayag0@gmail.com>
2023-04-16 14:54:05 +0300

    add signin route

--- src/routes/homePage.routes.js
+++ src/routes/homePage.routes.js
@@ -1,6 +1,6 @@
 import express from "express"
 
-// import { User } from "../models/userScheme.js"
+import { User } from "../models/userScheme.js"
 const router = express.Router();
 
 router.get('/', (req, res)=>{
@@ -27,5 +27,19 @@ router.get('/', (req, res)=>{
 //         console.log(error);
 //     }
 // });
+
+router.post('/signin', async (req, res)=>{
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if(user){
+        if(user.password === password){
+            res.send(user);
+        }else{
+            res.send('Wrong password');
+        }
+    }else{
+        res.send('User not found');
+    }
+});
 
 export default router;
