69ab51524edeafbf93518fe37cc46917991246a2
Tal Mekler <tal.mekler11@gmail.com>
2023-05-07 10:17:37 +0300

    update API

--- src/services/category.services.js
+++ src/services/category.services.js
@@ -24,5 +24,87 @@ const updateCategory = async (category)=> {
     return await Category.findOneAndUpdate(category.title, category)
 }
 
-const CategoryApi = {getAllCategories, getCategoryByTitle, updateCategory, addCatalog, getCategoryById};
+/**
+ * @param {*} req -> include {catlog: new Category}
+ * @param {*} res 
+ * @returns 
+ */
+const add = async (req, res) => {
+    try {
+        const {catalog} = req.body
+        const DB_catalog = await catalog.save()
+        if(DB_catalog){
+            res.status(200)
+            return DB_catalog._id
+        }    
+    } catch (error) {
+        console.log(error);
+        res.status(400)
+    }
+}
+
+const getById = async (req, res) => {
+    const {id} = req.body
+    const catalog = await Category.findById(id)
+    if(catalog) {
+        res.status(200)
+        return catalog;
+    }
+    console.log(error);
+    res.status(400)
+}
+
+const getByName = async (req, res) => {
+    const {name} = req.body
+    const catalog = await Category.findOne({name})
+    if(catalog) {
+        res.status(200)
+        return catalog;
+    }
+    console.log(error);
+    res.status(400)
+}
+
+const getAll = async (req, res) => {
+    const catalogs = await Category.find()
+    if(catalogs) {
+        res.status(200)
+        return catalogs
+    }
+    console.log(error);
+    res.status(400)
+}
+
+const update = (req, res) => {
+    try {
+        const {id, catalog} = req.body
+        const updatedCatalog = Category.findOneAndUpdate(_id == id, catalog);
+        res.status(200)
+        return updatedCatalog._id
+    } catch (error) {
+        console.log(error);
+        res.status(400)
+    }
+}
+
+const remove = (req, res) => {
+    try {
+        const {id} = req.body
+        Category.deleteOne(_id == id)
+        res.status(200)
+    } catch (error) {
+        console.log(error);
+        res.status(400)
+    }
+}
+
+
+
+
+
+
+
+
+// const CategoryApi = {getAllCategories, getCategoryByTitle, updateCategory, addCatalog, getCategoryById};
+const CategoryApi = {add, getAll, getById, getByName, remove, update};
 export default CategoryApi;
