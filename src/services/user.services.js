431f7993f16f72b887128d9f5810c7b7cbcdfd12
YuvalOfek1 <yuvalofek97@gmail.com>
2023-05-10 11:09:20 +0300

    fixed user services

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,57 +1,51 @@
 import {User} from '../models/userScheme'
 
-const getAllUsers = async () => {
-    return await User.find();
+const getAll = async () => {
+    const users = await User.find();
+    if(users){
+        return users;
+    }
+    console.log("No users found in getAll");
 }
 
-const getUserById = async (id) => {
+const getById = async (id) => {
     return await User.findById(id);
 }
 
-const getUserByEmail = async (email) => {
+const getByEmail = async (email) => {
     return await User.findOne({email});
 }
 
-const addUser = async (firstName, lastName, email, password, profilePic, phoneNumber, address, role, orders, numOfOrders, creditCardNumber) =>{
+const add = async (user) => {
     try{
-        const user= new User({
-            firstName,
-            lastName,
-            email,
-            password,
-            profilePic,
-            phoneNumber,
-            address,
-            role,
-            orders,
-            numOfOrders,
-            creditCardNumber
-        });
+        const DB_user = await user.save();
+        if(DB_user){
+            return DB_user._id;
+        }
+    } catch(err){
+        console.log(err);
+    }
+    
+}
+
+const removeById = (id) => {
+    try{
+        User.deleteOne(_id==id);
+    } catch(err){
+        console.log(err);
     }
 }
 
-const removeUser = async (email) => {
-    await User.deleteOne({email});
+const update = (id, user) =>{
+    try{
+        const updatedUser = User.findByIdAndUpdate(id, user);
+        return updatedUser._id;
+    } catch(err){
+        console.log(err);
+    }
+    
 }
 
-const updateUser = async (firstName, lastName, email, newEmail, password, profilePic, phoneNumber, address, role, orders, numOfOrders, creditCardNumber) =>{
-    await User.findOneAndUpdate({email},
-        {
-            firstName,
-            lastName,
-            email:newEmail,
-            password,
-            profilePic,
-            phoneNumber,
-            address,
-            role,
-            orders,
-            numOfOrders,
-            creditCardNumber
-
-        })
-}
-
-const UserApi = {getAllUsers, getUserById, getUserByEmail, addUser, removeUser, updateUser};
+const UserApi = {getAll, getById, getByEmail, add, removeById, update};
 
 export default UserApi;
