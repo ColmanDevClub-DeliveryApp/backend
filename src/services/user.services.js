fe30c0ee5f6e7b5b427327dccf4a3ffafaabda2f
YuvalOfek1 <yuvalofek97@gmail.com>
2023-05-10 14:28:42 +0300

    added docs to users api

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,5 +1,8 @@
 import {User} from '../models/userScheme'
 
+/**
+ * @returns type Array | all users in database
+ */
 const getAll = async () => {
     const users = await User.find();
     if(users){
@@ -8,14 +11,26 @@ const getAll = async () => {
     console.log("No users found in getAll");
 }
 
+/**
+ * @param {*} id type String
+ * @returns User object or null
+ */
 const getById = async (id) => {
     return await User.findById(id);
 }
 
+/**
+ * @param {*} id type String
+ * @returns User object or null
+ */
 const getByEmail = async (email) => {
     return await User.findOne({email});
 }
 
+/**
+ * @param {*} user type User | user that will be added to database
+ * @returns type String | the id of the added user
+ */
 const add = async (user) => {
     try{
         const DB_user = await user.save();
@@ -28,6 +43,9 @@ const add = async (user) => {
     
 }
 
+/**
+ * @param {*} id type String | the id of the user that will be removed
+ */
 const removeById = (id) => {
     try{
         User.deleteOne(_id==id);
@@ -36,6 +54,11 @@ const removeById = (id) => {
     }
 }
 
+/**
+ * @param {*} id type String | the id of the user that will be changed
+ * @param {*} user type User | new user that will be updated
+ * @returns type String | the id of the updated user
+ */
 const update = (id, user) =>{
     try{
         const updatedUser = User.findByIdAndUpdate(id, user);
