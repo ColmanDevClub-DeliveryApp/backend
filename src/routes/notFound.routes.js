456835b431389b918669a4c34c1247341e5776df
Idan Asayag <idanasayag0@gmail.com>
2023-05-14 10:50:57 +0300

    notFoundPage

--- src/routes/notFound.routes.js
+++ src/routes/notFound.routes.js
@@ -3,7 +3,7 @@ import express from "express"
 const router = express.Router();
 
 router.get('*', (req, res)=>{
-    res.status(404).sendFile(process.cwd() + '/src/pageNotFound.html');
+    res.status(404).sendFile(process.cwd() + '/src/notFoundPage/index.html');
 });
 
 export default router;
